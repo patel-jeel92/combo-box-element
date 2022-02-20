bca3851c601ecc91415521764940265d62dd90a6
Jeel Patel <jeel@Jeels-MacBook-Pro.local>
2022-02-20 13:46:40 -0500

    Move datalist inside custom element

--- combo-box.js
+++ combo-box.js
@@ -7,6 +7,31 @@ class ComboBox extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const template = document.createElement("div");
 
+    template.innerHTML = `
+    <style>
+        input::-webkit-calendar-picker-indicator {
+            opacity: 100;
+        }
+        input {
+            background-color: lightgray;
+            padding: auto;
+            height: 30px;
+            width: 200px;
+            border-radius: 5px;
+        }    
+    </style>
+    <input list="fruits" id="fruit-choice" name="fruit-choice" placeholder="Choose a Fruit:" />
+    <datalist id="fruits">
+        <option value="Apple"></option>
+        <option value="Banana"></option>
+        <option value="Blueberry"></option>
+        <option value="Mango"></option>
+        <option value="Watermelon"></option>
+        <option value="Grape"></option>
+        <option value="Orange"></option>
+        <option value="Pineapple"></option>
+    </datalist>
+    `;
     shadow.appendChild(template);
   }
 
