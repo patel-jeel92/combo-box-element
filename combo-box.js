f3d3d479a409817615b48a093787942329561711
Jeel Patel <jeel@Jeels-MacBook-Pro.local>
2022-02-20 14:42:03 -0500

    Refactor custom element to use dynamic data

--- combo-box.js
+++ combo-box.js
@@ -7,6 +7,10 @@ class ComboBox extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
     const template = document.createElement("div");
 
+    // user getter to get items
+    const listItems = this.items;
+
+    // add html to the template
     template.innerHTML = `
     <style>
         input::-webkit-calendar-picker-indicator {
@@ -20,23 +24,48 @@ class ComboBox extends HTMLElement {
             border-radius: 5px;
         }    
     </style>
-    <input list="fruits" id="fruit-choice" name="fruit-choice" placeholder="Choose a Fruit:" />
-    <datalist id="fruits">
-        <option value="&#127822 Apple"></option>
-        <option value="&#127820 Banana"></option>
-        <option value="&#127817 Watermelon"></option>
-        <option value="&#127815 Grape"></option>
-        <option value="&#127818 Orange"></option>
-        <option value="&#127821 Pineapple"></option>
-        <option value="&#127827 Strawberry"></option>
-        <option value="&#127826 Cherry"></option>
-        
+    <input list="custom-items" name="combo-box" placeholder="${
+      this.placeholder
+    }" />
+    <datalist id="custom-items">
+    ${listItems
+      .map(
+        (item) => `
+        <option value="${item}">${item}</option>
+        `
+      )
+      .join("")}
     </datalist>
     `;
     shadow.appendChild(template);
   }
 
+  // fires after the element has been attached to the DOM
   connectedCallback() {}
+
+  // gather data from element attributes
+  get placeholder() {
+    return this.getAttribute("placeholder") || "Choose an item:";
+  }
+
+  get id() {
+    return this.getAttribute("id") || "combo-box-id";
+  }
+
+  get items() {
+    const items = [];
+    let inputItems = this.getAttribute("items") || [];
+    inputItems.forEach((item) => {
+      items.push(item);
+    });
+
+    return items;
+  }
+
+  set items(value) {
+    this.items = value;
+  }
 }
 
+// let the browser know about the custom element
 window.customElements.define("combo-box", ComboBox);
